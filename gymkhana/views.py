52cee3322351f8ef3f32c9c740aa9280d859a622
Geetmanik <geetmanik2@gmail.com>
2024-02-03 15:41:34 +0530

    adding dynmic images in home screen

--- gymkhana/views.py
+++ gymkhana/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
-from .models import meeting,ugSenator,pgSenator,facultyExecutivebodie,studentExecutivebodie,upcomingEvent,GirlSenator
+from .models import meeting,ugSenator,pgSenator,facultyExecutivebodie,studentExecutivebodie,upcomingEvent,GirlSenator,landingPageImages
 
 # from rest_framework.decorators import api_view
 
 def home(request):
     faculty=facultyExecutivebodie.objects.all()
     student=studentExecutivebodie.objects.all()
+    images=landingPageImages.objects.all()
     events=upcomingEvent.objects.all()
-    return render(request,'main/home.html',{'faculty':faculty,'student':student,'events':events})
+    return render(request,'main/home.html',{'faculty':faculty,'student':student,'events':events,'images':images})
     
 
 
