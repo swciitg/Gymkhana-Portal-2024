4969f9b125458db05a92bb2da4342db9e88c9723
Siddhant <103687316+Sid2728@users.noreply.github.com>
2024-07-28 11:29:48 +0530

    Update urls.py

--- gymkhana_portal/urls.py
+++ gymkhana_portal/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
-baseurl = "gymkhana_2024/"
+baseurl = "gymkhana/"
 urlpatterns = [
     path(baseurl+'admin/', admin.site.urls),
     path('', include('gymkhana.urls')),
