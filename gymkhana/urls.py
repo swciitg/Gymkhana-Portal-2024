524d48b540895042e74cc9afd8c60d8022462660
Siddhant <103687316+Sid2728@users.noreply.github.com>
2024-08-06 11:05:45 +0530

    Update urls.py

--- gymkhana/urls.py
+++ gymkhana/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 baseurl = "gymkhana/"
 urlpatterns = [
-    path(baseurl+'', views.home, name = 'home-page'),
+    path('stud/'+baseurl+'', views.home, name = 'home-page'),
     path('stud/'+baseurl+'senate/',views.Senate,name='senate-page'),
     path('stud/'+baseurl+'minutes/',views.minutes,name='minutes-page'),
     path('stud/'+baseurl+'powersenate/',views.powersenate,name='powersenate'),
